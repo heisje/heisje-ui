74d2deecdac6887569e31269accf0ea7816fe639
heisje <heeje1996@gmail.com>
2023-08-28 18:53:45 +0900

    prettier test

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,7 +1,13 @@
-export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
+export default function RootLayout({ children }:        { children: React.ReactNode }): JSX.Element {
   const a = 'a';
+
+
   return (
     <html lang="en">
+
+
+
+
       <body>
         {children}
         {a}
